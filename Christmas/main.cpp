cb6ef54fb6233a74b626c1c3fed0f5b99915f159
Alaa Amr Mansour <alaa.amr1@hotmail.com>
2021-11-26 02:00:51 +0200

    Snowman & Trees

--- Christmas/main.cpp
+++ Christmas/main.cpp
@@ -22,11 +22,11 @@ public:
         z = _z;
     }
 
-    Vector3f operator+(Vector3f &v) {
+    Vector3f operator+(Vector3f v) {
         return Vector3f(x + v.x, y + v.y, z + v.z);
     }
 
-    Vector3f operator-(Vector3f &v) {
+    Vector3f operator-(Vector3f v) {
         return Vector3f(x - v.x, y - v.y, z - v.z);
     }
 
@@ -47,6 +47,7 @@ public:
     }
 };
 
+
 class Camera {
 public:
     Vector3f eye, center, up;
@@ -59,28 +60,25 @@ public:
 
     void moveX(float d) {
         Vector3f right = up.cross(center - eye).unit();
-        eye = (eye + right) * d;
-        center = (center + right) * d;
+        eye = eye + right * d;
+        center = center + right * d;
     }
 
     void moveY(float d) {
-        Vector3f zk= up.unit();
-        eye = (eye + zk) * d;
-        center = (center + zk) * d;
+        eye = eye + up.unit() * d;
+        center = center + up.unit() * d;
     }
 
     void moveZ(float d) {
         Vector3f view = (center - eye).unit();
-        eye = (eye + view) * d;
-        center = (center + view) * d;
+        eye = eye + view * d;
+        center = center + view * d;
     }
 
     void rotateX(float a) {
         Vector3f view = (center - eye).unit();
         Vector3f right = up.cross(view).unit();
-        Vector3f cc =view * cos(DEG2RAD(a));
-        Vector3f ss = up * sin(DEG2RAD(a));
-        view = cc+ss;
+        view = view * cos(DEG2RAD(a)) + up * sin(DEG2RAD(a));
         up = view.cross(right);
         center = eye + view;
     }
@@ -88,9 +86,7 @@ public:
     void rotateY(float a) {
         Vector3f view = (center - eye).unit();
         Vector3f right = up.cross(view).unit();
-        Vector3f cc =view * cos(DEG2RAD(a));
-        Vector3f ss = right * sin(DEG2RAD(a));
-        view = cc+ss;
+        view = view * cos(DEG2RAD(a)) + right * sin(DEG2RAD(a));
         right = view.cross(up);
         center = eye + view;
     }
@@ -105,6 +101,7 @@ public:
 };
 
 Camera camera;
+
 
 void drawWall(double thickness) {
     glPushMatrix();
@@ -159,80 +156,304 @@ void setupLights() {
 void setupCamera() {
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    gluPerspective(100, 640 / 480, 0.001, 100);
+    gluPerspective(100, 640 / 480, 0.0001, 100);
 
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
     camera.look();
 }
+void Floor(){
+    glColor3f(0.3f, 0.0f, 0.0f);
+    glPushMatrix();
+    glScaled(10, 0.3, 10);
+    glRotated(45, 0, 1, 0);
+    glutSolidCube(0.2);
+    glPopMatrix();
+}
 
-void drawSnowMan(float x, float y, float z) {
-
+void Presents(){
 glPushMatrix();
-
- glColor3f(1.0f, 1.0f, 1.0f);
-// Draw Body
-glPushMatrix();
-glTranslatef(x, y-0.4f, z);
-glutSolidSphere(0.33f,20,20);
-glPopMatrix();
     
 glPushMatrix();
-glTranslatef(x, y+0.25f, z);
-glColor3f(1.0f, 0.0f , 0.0f);
-glRotatef(90, 1, 0, 0);
-glutSolidTorus(0.05f, 0.09f, 20, 20);
+glTranslatef(0.5f, 0.13f, -0.5);
+glRotated(45,0, 1, 0);
+glutSolidCube(0.1);
 glPopMatrix();
-    
-glPushMatrix();
-glColor3f(1.0f, 1.0f , 1.0f);
-glTranslatef(x ,y, z);
-glutSolidSphere(0.20f,30,20);
-glPopMatrix();
-    
-// Draw Head
- glPushMatrix();
- glTranslatef(x, y+0.35f, z);
- glutSolidSphere(0.10f,20,20);
- glPopMatrix();
 
-// Draw Hat
 glPushMatrix();
-glTranslatef(x+0.18, y+0.52, 0.18f);
-glColor3f(1.0f, 0.0f , 0.0f);
+glColor3f(1.0f, 1.0f, 0.0f);
+glTranslatef(0.5f, 0.14f, -0.5);
+glRotated(45,0, 1, 0);
+glScalef(1.1, 1.2, 0.1);
+glutSolidCube(0.1);
+glPopMatrix();
+
+glPushMatrix();
+glColor3f(1.0f, 1.0f, 0.0f);
+glTranslatef(0.5f, 0.14f, -0.5);
+glRotated(-45,0, 1, 0);
+glScalef(1.1, 1.2, 0.1);
+glutSolidCube(0.1);
+glPopMatrix();
+
+glPushMatrix();
+glColor3f(1.0f, 0.0f, 1.0f);
+glTranslatef(0.5f, 0.22f, -0.5);
+glutSolidSphere(0.015, 5, 5);
+glPopMatrix();
+    
+glPopMatrix();
+
+}
+
+void Tree(){
+    
+// Tree 1
+glPushMatrix();
+glColor3f(0.0f, 0.6f, 0.0f);
+
+glPushMatrix();
+glTranslatef(0.15f, 0.13f, -0.5);
 glRotated(-90,1, 0, 0);
-glutSolidCone(0.055f,0.15f,10,2);
+glutSolidCone(0.3f,0.7f,10,2);
+glPopMatrix();
+
+glPushMatrix();
+glTranslatef(0.15f, 0.55f, -0.5);
+glRotated(-90,1, 0, 0);
+glutSolidCone(0.22f,0.5f,10,2);
+glPopMatrix();
+    
+glPushMatrix();
+glTranslatef(0.15f, 0.85f, -0.5);
+glRotated(-90,1, 0, 0);
+glutSolidCone(0.14f,0.5f,10,2);
+glPopMatrix();
+    
+ // Tree 1 Christmas Decoration
+glPushMatrix();
+glColor3f(1.0f, 0.0f, 1.0f);
+glTranslatef(-0.2f, 0.185f, -0.3);
+glTranslatef(0.56, 0.61f, 0.195);
+glutSolidSphere(0.01f,10,10);
 glPopMatrix();
         
+    
 glPushMatrix();
-glColor3f(1.0f, 1.0f , 1.0f);
-glTranslatef(x+0.18, y+0.65f, 0.18f);
-glutSolidSphere(0.03f,20,20);
-glRotated(50,0, 1, 0);
+glColor3f(0.0f, 0.6f, 0.0f);
+
+glPushMatrix();
+glTranslatef(-0.5f, 0.13f, 0.15);
+glRotated(-90,1, 0, 0);
+glutSolidCone(0.3f,0.7f,10,2);
+glPopMatrix();
+
+glPushMatrix();
+    glTranslatef(-0.5f, 0.55f, 0.15);
+glRotated(-90,1, 0, 0);
+glutSolidCone(0.22f,0.5f,10,2);
 glPopMatrix();
     
+glPushMatrix();
+glTranslatef(-0.5f, 0.86f, 0.15);
+glRotated(-90,1, 0, 0);
+glutSolidCone(0.14f,0.5f,10,2);
+glPopMatrix();
+    
+}
+void drawSnowMan() {
+
+//glPushMatrix();
+
+ glColor3f(1.0f, 1.0f, 1.0f);
+
+
+// Draw Body
+glPushMatrix();
+glTranslatef(0.5, 0.4f, 0.1);
+    glScaled(1, 1.3, 1);
+glutSolidSphere(0.1f,20,20);
+glPopMatrix();
+
+glPushMatrix();
+glColor3f(1.0f, 1.0f , 1.0f);
+glTranslatef(0.5 ,0.58, 0.13);
+glutSolidSphere(0.065f,30,20);
+glPopMatrix();
+    
+glPushMatrix();
+glColor3f(0.0f,0.0f,0.0f);
+glTranslatef(0.56, 0.58f, 0.195);
+glutSolidSphere(0.01f,10,10);
+glPopMatrix();
+    
+glPushMatrix();
+glColor3f(0.0f,0.0f,0.0f);
+glTranslatef(0.56, 0.61f, 0.195);
+glutSolidSphere(0.01f,10,10);
+glPopMatrix();
+    
+   
+// Draw Scarf
+glPushMatrix();
+glTranslatef(0.5, 0.65f, 0.14);
+glColor3f(1.0f, 0.0f , 0.0f);
+glRotatef(90, 1, 0, 0);
+glutSolidTorus(0.02f, 0.03f, 20, 20);
+glPopMatrix();
+
+// Draw Head
+ glColor3f(1.0f, 1.0f, 1.0f);
+ glPushMatrix();
+ glTranslatef(0.5, 0.7f, 0.14);
+ glutSolidSphere(0.04f,20,20);
+ glPopMatrix();
+
+//Draw Hat
+glPushMatrix();
+glTranslatef(0.5f, 0.73f, 0.14);
+glColor3f(1.0f, 0.0f , 0.0f);
+glRotated(-90,1, 0, 0);
+glutSolidCone(0.03f,0.06f,10,2);
+glPopMatrix();
+    
+glPushMatrix();
+glColor3f(1.0f,1.0f,1.0f);
+glTranslatef(0.5, 0.8f, 0.14);
+glutSolidSphere(0.01f,10,10);
+glPopMatrix();
+
+
 //Draw Nose
 glPushMatrix();
 glColor3f(1.0f, 0.0f , 0.0f);
-glTranslatef(x+0.18, y+0.45f, 0.18f);
+glTranslatef(0.5, 0.7f, 0.14);
 glRotatef(45.0f,0.0f, 1.0f, 0.0f);
-glutSolidCone(0.01f,0.18f,10,2);
+glutSolidCone(0.01f,0.1f,10,2);
 glPopMatrix();
-    
-    // Draw Eyes
-    glPushMatrix();
-    glColor3f(0.0f,0.0f,0.0f);
-    glTranslatef(x-0.15f, y+0.3f, 0);
-    glutSolidSphere(0.01f,10,10);
-    glPopMatrix();
+
+// Draw Eyes
 //glPushMatrix();
-//glTranslatef(x+0.18, 0.47f, 0);
+//glColor3f(0.0f,0.0f,0.0f);
+//glTranslatef(0.3, 0.71f, 0.14);
 //glutSolidSphere(0.01f,10,10);
 //glPopMatrix();
-    
+
+//glPushMatrix();
+//glTranslatef(0.5, 0.8f, 0.14);
+//glutSolidSphere(0.01f,10,10);
+//glPopMatrix();
+
 glPopMatrix();
 }
 
+void LeftFence(){
+    glColor3f(0.6f, 0.0f, 0.0f);
+    double z= 0.4;
+    double x=-0.85;
+    for( int i=0; i<7; i++ ,z+=0.15, x+=0.15 ){
+    glPushMatrix();
+    glTranslated(x, 0.4, z);
+    glScaled(0.1, 1, 0.1);
+//    glRotated(90, 1, 0, 1);
+    glRotated(45, 0, 1, 0);
+    glutSolidCube(0.6);
+    glPopMatrix();
+}
+    
+    glPushMatrix();
+    glTranslated(-0.66, 0.4, 0.55);
+    glRotated(45, 0, 1, 0);
+    glRotated(90, 1, 0, 0);
+    glScaled(0.1, 4, 0.1);
+    glutSolidCube(0.5);
+    glPopMatrix();
+}
+
+void RightFence(){
+    glColor3f(0.6f, 0.0f, 0.0f);
+    double z= -0.85;
+    double x=0.35;
+    for( int i=0; i<7; i++ ,z+=0.15, x+=0.15 ){
+    glPushMatrix();
+    glTranslated(x, 0.4, z);
+    glScaled(0.1, 1, 0.1);
+//    glRotated(90, 1, 0, 1);
+    glRotated(45, 0, 1, 0);
+    glutSolidCube(0.6);
+    glPopMatrix();
+}
+    glPushMatrix();
+    glTranslated(0.6, 0.4, -0.55);
+    glRotated(45, 0, 1, 0);
+    glRotated(90, 1, 0, 0);
+    glScaled(0.1, 3.7, 0.1);
+    glutSolidCube(0.5);
+    glPopMatrix();
+}
+
+void BackFence(){
+    glColor3f(0.6f, 0.0f, 0.0f);
+    glPushMatrix();
+    glTranslated(-1.15, 0.4, 0.1);
+    glScaled(0.18, 1, 0.18);
+//    glRotated(90, 1, 0, 1);
+    glRotated(-45, 0, 1, 0);
+    glutSolidCube(0.6);
+    glPopMatrix();
+    
+    glPushMatrix();
+    glTranslated(-0.95, 0.4, -0.1);
+    glScaled(0.18, 1, 0.18);
+    glRotated(-45, 0, 1, 0);
+    glutSolidCube(0.6);
+    glPopMatrix();
+    
+    glPushMatrix();
+    glTranslated(-0.75, 0.4, -0.3);
+    glScaled(0.18, 1, 0.18);
+    glRotated(-45, 0, 1, 0);
+    glutSolidCube(0.6);
+    glPopMatrix();
+    
+    glPushMatrix();
+    glTranslated(-0.55, 0.4, -0.5);
+    glScaled(0.18, 1, 0.18);
+    glRotated(-45, 0, 1, 0);
+    glutSolidCube(0.6);
+    glPopMatrix();
+    
+    glPushMatrix();
+    glTranslated(-0.35, 0.4, -0.7);
+    glScaled(0.18, 1, 0.18);
+    glRotated(-45, 0, 1, 0);
+    glutSolidCube(0.6);
+    glPopMatrix();
+    
+    glPushMatrix();
+    glTranslated(-0.15, 0.4, -0.9);
+    glScaled(0.18, 1, 0.18);
+    glRotated(-45, 0, 1, 0);
+    glutSolidCube(0.6);
+    glPopMatrix();
+    
+    glPushMatrix();
+    glTranslated(0.05, 0.4, -1.1);
+    glScaled(0.18, 1, 0.18);
+    glRotated(-45, 0, 1, 0);
+    glutSolidCube(0.6);
+    glPopMatrix();
+    
+    
+    glTranslated(-0.5, 0.4, -0.45);
+    glRotated(-45, 0, 1, 0);
+    glRotated(90, 1, 0, 0);
+    glScaled(0.1, 4, 0.1);
+    glutSolidCube(0.5);
+    glPopMatrix();
+    
+ 
+}
 void Display() {
     setupCamera();
     setupLights();
@@ -240,7 +461,7 @@ void Display() {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
    
-    glPushMatrix();
+//    glPushMatrix();
 //    glTranslated(0.6, 0.38, 0.5);
 //    glRotated(30, 0, 1, 0);
 //    glutSolidTeapot(0.08);
@@ -249,13 +470,22 @@ void Display() {
 //    glTranslated(0.25, 0.42, 0.35);
 //    glutSolidSphere(0.1, 15, 15);
 //    glPopMatrix();
-    drawSnowMan(0.0f,0.0f,0.0f);
+//
+      Floor();
+    Tree();
+    drawSnowMan();
+    Presents();
+      LeftFence();
+      RightFence();
+    BackFence();
+   
+//      drawSnowMan();
 //    glPushMatrix();
 //    glTranslated(0.4, 0.0, 0.4);
 //    drawTable(0.6, 0.02, 0.02, 0.3);
 //    glPopMatrix();
 //    drawWall(0.02);
-    glPushMatrix();
+//    glPushMatrix();
 //    glRotated(0, 0, 1.0, 0);
 //    drawWall(0.02);
 //    glPopMatrix();
@@ -296,6 +526,8 @@ void Keyboard(unsigned char key, int x, int y) {
 
     glutPostRedisplay();
 }
+
+
 void Special(int key, int x, int y) {
     float a = 1.0;
 
